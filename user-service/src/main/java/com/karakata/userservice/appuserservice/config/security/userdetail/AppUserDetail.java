f8b7dd756167171e838cd480276ef430f2ca222d
sunlolafakolujo <dapafol@live.com>
2024-02-20 08:27:43 +0100

    Created userdetails implementation class

--- user-service/src/main/java/com/karakata/userservice/appuserservice/config/security/userdetail/AppUserDetail.java
+++ user-service/src/main/java/com/karakata/userservice/appuserservice/config/security/userdetail/AppUserDetail.java
@@ -1,2 +1,49 @@
-package com.karakata.userservice.appuserservice.config.security.userdetail;public class AppUserDetail {
+package com.karakata.userservice.appuserservice.config.security.userdetail;
+
+import com.karakata.userservice.appuserservice.appuser.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public record AppUserDetail(User user) implements UserDetails {
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
