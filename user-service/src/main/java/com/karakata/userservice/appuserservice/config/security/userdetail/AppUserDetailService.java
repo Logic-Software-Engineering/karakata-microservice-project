6d9479c8c86e8d2d56ecea33b634ab95b15169ab
sunlolafakolujo <dapafol@live.com>
2024-02-20 08:29:12 +0100

    UserdetailsService implentation class

--- user-service/src/main/java/com/karakata/userservice/appuserservice/config/security/userdetail/AppUserDetailService.java
+++ user-service/src/main/java/com/karakata/userservice/appuserservice/config/security/userdetail/AppUserDetailService.java
@@ -1,2 +1,23 @@
-package com.karakata.userservice.appuserservice.config.security.userdetail;public class AppUserDetailService {
+package com.karakata.userservice.appuserservice.config.security.userdetail;
+
+import com.karakata.userservice.appuserservice.appuser.exception.UserNotFoundException;
+import com.karakata.userservice.appuserservice.appuser.model.User;
+import com.karakata.userservice.appuserservice.appuser.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String searchKey) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmailOrMobile(searchKey,searchKey,searchKey)
+                .orElseThrow(()->new UserNotFoundException("User with username or email or mobile "+searchKey+" not found"));
+        return new AppUserDetail(user);
+    }
 }
